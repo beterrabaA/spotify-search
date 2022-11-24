a7a29b12e8087fce8b7e675dafa5d8173c2a075c
Lerpardo <ls6182315@gmail.com>
2022-11-24 15:43:27 -0300

    container card add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap'
 import { useState, useEffect } from 'react'
 
+const CLIENT_ID = '61f9ced8db7f496d9a3c7bd711849dba';
+const CLIENT_SECRET = 'e48477dcf77942fdab00d08f044bab25';
+
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
@@ -24,6 +27,18 @@ function App() {
             Search
           </Button>
         </InputGroup>
+      </Container>
+      <Container>
+        <Row className='mx-2 row row-cols-4'>
+          <Card>
+            <Card.Img src='#' />
+            <Card.Body>
+              <Card.Title>
+                Album Name Here
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
       </Container>
     </div>
   );
