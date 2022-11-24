ab5bad0aefb6f7ebeb442c2dd115682ed6033bf0
Lerpardo <ls6182315@gmail.com>
2022-11-24 15:54:55 -0300

    api request token config

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ const CLIENT_SECRET = 'e48477dcf77942fdab00d08f044bab25';
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    // API Access Token
+    const authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `grant_type=client_credentials@client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters).then(result => result.json()).then(data => console.log(data))
+  }, [])
+
   return (
     <div className="App">
       <Container>
